f5e3064bfe2f51db9fa390cd09cfc519b3defa3f
Mr-Magic1 <vishwakarmakailash123@gmail.com>
2026-06-12 09:26:51 +0530

    Pixel decresed

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,22 @@ const App = () => {
 
   const [image, setimage] = useState([])
   const [pag, setpag] = useState(1)
+  const [loading, setLoading] = useState(false);
 
   async function getData(){
+    setLoading(true)
     let url=`https://picsum.photos/v2/list?page=${pag}&limit=100`
     let res=await fetch(url)
     let data=await res.json()
-    let copy_image=[]
-    data.forEach(element => {
-      copy_image.push( {link : element.download_url , author : element.author} )
-    });
-    setimage(copy_image)
+    setimage(
+      data.map(element => ({
+        id: element.id,
+        link : element.download_url,
+        author: element.author
+      }))
+    );
+    setLoading(false)
+    console.log(image)
   }
 
   useEffect(() => {
@@ -39,23 +45,25 @@ const App = () => {
       <nav className='bg-red-400 h-15 flex justify-center items-center'>
         <h1 className='italic font-medium text-2xl'>Welcome to Jhakkas React Gallery</h1>
       </nav>
-      <div className='flex flex-wrap gap-5 p-6 justify-around overflow-scroll h-[80vh]' >
-        {image.map((elem,id)=>{
-          return <Image_Card key={id} link={elem.link} author={elem.author}/>
-        })}
+      <div className='flex flex-wrap gap-5 p-6 justify-around overflow-scroll h-[80vh]'>
+        {loading ? (
+          <h1 className='text-3xl font-bold'>Loading...</h1>) : ( image.map((elem) => (
+                                                      <Image_Card key={elem.id} id={elem.id} link={elem.link} author={elem.author}/>
+          ))
+        )}
       </div>
       <div className="flex page h-20 justify-center items-center gap-10">
         <button 
             onClick={PageDecrement} 
             disabled={pag===1}
             style={{opacity : pag===1 ? 0.5 : 1}}
-            className='bg-yellow-400 active:scale-110 h-10 w-20 font-medium text-2xl rounded-2xl text-black'>Prev</button>
+            className='bg-yellow-400 cursor-grab active:scale-110 h-10 w-20 font-medium text-2xl rounded-2xl text-black'>Prev</button>
         <h1>Page  {pag}</h1>
         <button 
         onClick={PageIncrement} 
         disabled={pag===199}
             style={{opacity : pag===199 ? 0.5 : 1}}
-        className='bg-yellow-400  active:scale-110 h-10 w-20 font-medium text-2xl rounded-2xl text-black'>Next</button>
+        className='bg-yellow-400 cursor-grab active:scale-110 h-10 w-20 font-medium text-2xl rounded-2xl text-black'>Next</button>
       </div>
     </div>
   )
--- src/components/Image_Card.jsx
+++ src/components/Image_Card.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Image_Card = (props) => {
   return (
     <div>
-      <img src={props.link} className='h-70 w-70 object-cover' />
+      <img
+        src={`https://picsum.photos/id/${props.id}/300/300`}
+        className='h-70 w-70 object-cover cursor-pointer'
+        loading='lazy'
+        onClick={() => window.open(props.link, "_blank")}
+        alt={props.author}
+      />
       <h2>Author : {props.author} </h2>
     </div>
   )
